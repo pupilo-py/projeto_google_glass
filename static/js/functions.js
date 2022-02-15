7e65069d9c087e2b7b6d440e4b566cdbcda00896
pupilo-py <jv.13wap@gmail.com>
2022-02-15 20:33:21 -0300

    Desing da página home

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,3 +1,13 @@
-function name(params) {
-    
+const btnMobile = window.document.querySelector("#btn-mobile")
+btnMobile.addEventListener("click", trocarMenu)
+btnMobile.addEventListener("touchstart", trocarMenu)
+
+function trocarMenu(event) {
+
+    if (event.type === "touchstart") {
+        event.preventDefault()
+    }
+
+    const nav = window.document.querySelector("nav")
+    nav.classList.toggle("active")
 }
